e0a55b7896e18b97aa7ce59afed00946f4d41dda
Artem <rrampage1991@gmail.com>
2022-08-11 19:52:53 +0300

    Одинадцатая задача

---
+++ Task_11/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(1, 1000);
+Console.WriteLine(number);
+int digitIndex = 2;
+int digit      = number.ToString()[digitIndex] - '0';
+if (number < 100)
+{
+    Console.Write("Нет третей цифры");
+}
+else
+{
+    Console.Write(digit);
+}
+
