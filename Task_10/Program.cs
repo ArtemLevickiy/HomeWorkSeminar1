4cbc158a403fb3e9b2016b2a9f1f71f2c7f9ab57
Artem <rrampage1991@gmail.com>
2022-08-11 19:39:17 +0300

    Десятая задача

---
+++ Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = 512;
+Console.WriteLine (number);
+Console.WriteLine ("Введите число: ");
+int userNamber = int.Parse(Console.ReadLine());
+
+if (number % userNamber == 0)
+{
+Console.Write("Цифра кратна 512");
+}
+else
+{
+    int N = number % userNamber;
+    Console.Write("Остаток = ");
+    Console.Write(N);
+}
