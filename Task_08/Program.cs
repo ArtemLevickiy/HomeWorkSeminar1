881f8f471044caefb5ba4372f928b403e94be3d4
Artem <rrampage1991@gmail.com>
2022-08-11 19:05:57 +0300

    Восьмая задача

---
+++ Task_08/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(9, 100);
+Console.WriteLine (number);
+int Max = 1;
+int numberA = number % 10;
+int numberB = number / 10;
+if (numberA > Max) Max = numberA;
+if (numberB > Max) Max = numberB;
+Console.Write("Max = ");
+Console.Write(Max);
