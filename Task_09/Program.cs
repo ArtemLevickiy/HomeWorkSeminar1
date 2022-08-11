484b6cf93cfd3beda72aaa572fe81c8bca0a6596
Artem <rrampage1991@gmail.com>
2022-08-11 19:23:00 +0300

    Девятая задача

---
+++ Task_09/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(99, 1000);
+Console.WriteLine (number);
+var str = number.ToString();
+number = int.Parse(str.Remove(str.Length - 2,1));
+Console.Write("Без второй цифры =");
+Console.Write(number);
