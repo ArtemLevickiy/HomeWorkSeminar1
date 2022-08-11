4f69d89fa790cb288856049ce6a0d014b8f97b25
Artem <rrampage1991@gmail.com>
2022-08-11 17:49:27 +0300

    Шестое задание

---
+++ Task_06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number  = Convert.ToInt32(Console.ReadLine());
+if(number < 1000)
+{
+    if(number > 99)
+    {
+     int lastDigit = number % 10;
+    Console.WriteLine("Последняя цифра " + lastDigit);
+    }  
+}  
