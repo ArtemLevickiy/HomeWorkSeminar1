068ab950f0ea083f7ee85ea4b0bc4ce8c9f0fda6
Artem <rrampage1991@gmail.com>
2022-08-11 17:46:09 +0300

    Пятое задание

---
+++ Task_05/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startNumber = -number;
+while(startNumber <= number)
+{
+    Console.Write(startNumber + " ");
+    startNumber++;
+}
