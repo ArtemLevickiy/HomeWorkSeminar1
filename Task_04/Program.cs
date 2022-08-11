4a94cf82bc50d91691e541c3f9922c0c1f2ae7f7
Artem <rrampage1991@gmail.com>
2022-08-11 17:40:43 +0300

    Четвертое задание

---
+++ Task_04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите число: ");
+int userNamber = int.Parse(Console.ReadLine());
+if (userNamber % 2 == 0)
+{
+    Console.Write("Чётное число");
+}
+else
+{
+   Console.Write("Нечётное число"); 
+}
