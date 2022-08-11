20e3244eb5f03af155d42258e5432bc38b120c92
Artem <rrampage1991@gmail.com>
2022-08-11 16:37:51 +0300

    ервая задача

---
+++ Task_01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int userNamber = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int userNamber2 = int.Parse(Console.ReadLine());
+
+if ((userNamber2 * userNamber2) == userNamber)
+{
+    Console.WriteLine ("Квадрат второго числа равен первому числу");
+}
+else
+{
+    Console.WriteLine ("Квадрат второго числа НЕ равен первому числу");
+}
